83824f8cf7df2152ce0b9c3e90fcdb209de8f8b1
Connor Latham <connor.lathampri@gmail.com>
2019-09-26 01:19:04 -0500

    made initial conditions into a class

--- initial_conditions.py
+++ initial_conditions.py
@@ -1,21 +1,31 @@
 from sympy import Matrix
+class State:
 
-# INITIAL CONDITIONS
-# position [m]
-x0 = 0
-y0 = 0
-z0 = 30
-# velocity [m/s]
-xd0 = 0
-yd0 = 0
-zd0 = 0
-# orientation [rad]
-thYaw0 = 0
-thPitch0 = 0
-thRoll0 = 0
-# angular rates [rad/s]
-thYawd0 = 0
-thPitchd0 = 0
-thRolld0 = 0
+	def __init__(self, pos0, vel0, ori0, ang0, t0 = 0):
+		# INITIAL CONDITIONS
+		# position [m]
+		self.x0 = pos0[0]
+		self.y0 = pos0[1]
+		self.z0 = pos0[2]
+		# velocity [m/s]
+		self.xd0 = vel0[0]
+		self.yd0 = vel0[1]
+		self.zd0 = vel0[2]
+		# orientation [rad]
+		self.thYaw0 = ori0[0]
+		self.thPitch0 = ori0[1]
+		self.thRoll0 = ori0[2]
+		# angular rates [rad/s]
+		self.thYawd0 = ang0[0]
+		self.thPitchd0 = ang0[1]
+		self.thRolld0 = ang0[2]
+		# time of the state
+		self.timeStart = t0
 
-initConditions = Matrix([x0, y0, z0, xd0, yd0, zd0, thYaw0, thPitch0, thRoll0, thYawd0, thPitchd0, thRolld0])
+pos0 = [1,1,1]
+vel0 = [1,1,1]
+ori0 = [1,1,1]
+ang0 = [1,1,1]
+
+state1 = State(pos0,vel0,ori0,ang0,t0 = 3)
+print(state1.timeStart)

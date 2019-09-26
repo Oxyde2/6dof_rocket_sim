134762af3767af26f627a983653c825ac6d3a066
Connor Latham <connor.lathampri@gmail.com>
2019-09-26 01:00:52 -0500

    added many more tests for forward euler

--- euler.py
+++ euler.py
@@ -1,16 +1,34 @@
 from numpy import array
+from numpy import any
+from numpy import iscomplex
+from numpy import ndim
 
 def eulerStep(tStep, xn, yn):
+	if (ndim(tStep) != 0):
+		return('tStep is not a scalar')
+	if (iscomplex(tStep)):
+		return('tStep is not real')
 	if (tStep < 0):
 		return('tStep is < 0')
-	if (tStep > .01):
+	if (tStep > .005):
 		print('be careful with large time steps')
 		
 	xn = array(xn)
 	yn = array(yn)
 	
+	if (xn.size == 0):
+		return('xn cannot be empty')
+	if(any(xn) == None):
+		return('xn cannot have any None')
+	if (yn.size == 0):
+		return('yn cannot be empty')
+	if(any(yn) == None):
+		return('yn cannot have any None')
+	if(iscomplex(xn).any()):
+		return('xn cannot have any imaginary numbers')
+	if(iscomplex(yn).any()):
+		return('yn cannot have any imaginary numbers')
+	
 	ynp1 = yn + tStep*xn
 	
 	return  ynp1
-	
-	
--- tests/test_euler.py
+++ tests/test_euler.py
@@ -22,6 +22,42 @@ class TestEulerStep(unittest.TestCase):
 			else:
 				incorrect = 0
 		self.assertFalse(incorrect, 'output array is predetermined to all be -1, and they are not')
+		
+	def test_mult_steps(self):
+		xn = -9.81
+		yn = 0
+		tn = 0
+		tf = 10
+		tStep = .005
+		while tn < 10:
+			ynp1 = eulerStep(tStep,xn,yn)
+			tn +=tStep
+			yn = ynp1
+		self.assertTrue((abs(yn-(-98.1)) < 10**-3), 'error should be relatively low for many iterations with a small tStep')
+		
+	def test_xn_empty(self):
+		self.assertEqual(eulerStep(.001,[],0),'xn cannot be empty')
+		
+	def test_yn_empty(self):
+		self.assertEqual(eulerStep(.001,0,[]),'yn cannot be empty')
+		
+	def test_xn_none(self):
+		self.assertEqual(eulerStep(.001,[None],0),'xn cannot have any None')
+	
+	def test_yn_none(self):
+		self.assertEqual(eulerStep(.001,0,[None]),'yn cannot have any None')
+	
+	def test_xn_imaginary(self):
+		self.assertEqual(eulerStep(.001,[5j],0),'xn cannot have any imaginary numbers')
+	
+	def test_yn_imaginary(self):
+		self.assertEqual(eulerStep(.001,0,[5j]),'yn cannot have any imaginary numbers')
+	
+	def test_tStep_imaginary(self):
+		self.assertEqual(eulerStep(5j,0,0),'tStep is not real')
+	
+	def test_tStep_list(self):
+		self.assertEqual(eulerStep([1,2,3],1,1),'tStep is not a scalar')
 
 if __name__ =='__main__':
 	unittest.main()
